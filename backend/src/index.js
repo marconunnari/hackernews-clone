d0cae8bbb49fa5bf23ec58c891ca319824f37422
Marco Nunnari <hello@marconunnari.com>
2022-04-10 13:27:20 +0200

    Backend: subscriptions and votes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,21 +2,28 @@ const fs = require("fs");
 const path = require("path");
 const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
+const { PubSub } = require("apollo-server");
 
 const { getUserId } = require("./utils");
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
+const Subscription = require("./resolvers/Subscription");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Vote = require("./resolvers/Vote");
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote,
 };
 
 const prisma = new PrismaClient();
+
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
@@ -25,6 +32,7 @@ const server = new ApolloServer({
     return {
       ...req,
       prisma,
+      pubsub,
       userId: req && req.headers.authorization ? getUserId(req) : null,
     };
   },
