ab48cb73d91f04f835e53768e6229d9481c951bc
Marco Nunnari <hello@marconunnari.com>
2022-04-11 20:46:23 +0200

    Frontend: search links

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,7 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import LinkList from "./LinkList";
 import CreateLink from "./CreateLink";
-import Login from './Login';
+import Login from "./Login";
+import Search from "./Search";
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LinkList />} />
           <Route path="/create" element={<CreateLink />} />
-          <Route path="/login" element={<Login/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
       </div>
     </div>
